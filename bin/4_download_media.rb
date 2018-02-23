cf83c0ae7a760e2a9ee5f7a543a74f365d2c9391
Charlie Egan <charlieegan3@users.noreply.github.com>
2018-02-23 22:44:42 +0000

    Add all media

--- bin/4_download_media.rb
+++ bin/4_download_media.rb
@@ -7,14 +7,18 @@ require "fileutils"
 Dir.glob("completed_json/*").shuffle.map do |file|
   data = JSON.parse(File.read(file))
 
-  puts data["post_url"]
-
   format = data["is_video"] == true ? "mp4" : "jpg"
 
   media_file_name = "media/#{file.split("/").last.gsub("json", format)}"
 
   next if File.exists?(media_file_name)
+  puts data["post_url"]
 
-  File.write(media_file_name, open(data["media_url"]).read)
-  FileUtils.touch media_file_name, mtime: data["timestamp"]
+  begin
+    File.write(media_file_name, open(data["media_url"]).read)
+    FileUtils.touch media_file_name, mtime: data["timestamp"]
+  rescue
+    puts "#{media_file_name} Failed"
+    File.delete(media_file_name) if File.exists?(media_file_name)
+  end
 end

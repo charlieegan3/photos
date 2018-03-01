78b5e6c0f5ad2aa78e900a1bf7095ffe8bf460b7
Charlie Egan <charlieegan3@users.noreply.github.com>
2018-03-01 22:53:20 +0000

    Run first task in docker
    Remove virtualenv stuff

--- bin/1_sync_raw.sh
+++ bin/1_sync_raw.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-instaLooter charlieegan3 looted_json/ -v -D -T {date}-{id} --new
+docker run -v "$(pwd)/looted_json:/out" -it python:alpine3.6 sh -c "pip install instaLooter && ls /out && instaLooter charlieegan3 /out -v -D -T {date}-{id} --new --time thisyear"
+
+git checkout looted_json

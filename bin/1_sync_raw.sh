e80641e2501fb4e29ded8e5841d1955f59d18689
Charlie Egan <charlieegan3@users.noreply.github.com>
2018-03-30 11:12:02 +0100

    Remove old instaLooter script
    It no longer works

--- bin/1_sync_raw.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -v "$(pwd)/looted_json:/out" -it python:alpine3.6 sh -c "pip install instaLooter && ls /out && instaLooter charlieegan3 /out -v -D -T {date}-{id} --time thisyear"
-
-git checkout looted_json

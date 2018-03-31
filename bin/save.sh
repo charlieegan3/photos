f75cc0268e7943f604cc8c79ffb76e045b88bb6c
Charlie Egan <charlieegan3@users.noreply.github.com>
2018-04-01 00:06:04 +0100

    Count json as media is now ignored

--- bin/save.sh
+++ bin/save.sh
@@ -6,6 +6,6 @@ if [ -z "$(git status --porcelain)" ]; then
   echo "Nothing to commit"
 else
   git add .
-  git -c user.name=automated -c user.email=git@charlieegan3.com commit -m "Add $(git status --porcelain | grep media | wc | awk '{ print $1 }') images"
+  git -c user.name=automated -c user.email=git@charlieegan3.com commit -m "Add $(git status --porcelain | grep looted_json | wc | awk '{ print $1 }') images"
   git push origin master
 fi

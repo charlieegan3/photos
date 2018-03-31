4da328f98b646384d2fc6a2c5d38048b2e21a738
Charlie Egan <charlieegan3@users.noreply.github.com>
2018-03-31 23:24:03 +0100

    Ignore places with missing location data

--- bin/3_save_locations.rb
+++ bin/3_save_locations.rb
@@ -3,24 +3,25 @@
 require "json"
 require "open-uri"
 
+locations_with_missing_data = %w(743759226 502239540)
+
 Dir.glob("completed_json/*").map do |file|
   JSON.parse(File.read(file))["location"]
 end.uniq.compact.each do |location|
   location_file_name = "locations/#{location["id"]}.json"
 
   next if File.exists?(location_file_name)
+  next if locations_with_missing_data.include?(location["id"])
 
   html = open("https://www.instagram.com/explore/locations/#{location["id"]}").read
   page_data = html.scan(/\{[^\n]+\}/).map { |r| JSON.parse(r) rescue nil }.compact.first
   location_data = page_data.dig(*%w(entry_data LocationsPage)).first["location"]
-
 
   begin
     location.merge!(lat: location_data["lat"], long: location_data["lng"])
     location.delete("has_public_page")
     File.write(location_file_name, JSON.pretty_generate(location))
   rescue
-    puts location["name"]
-    puts "Place missing location data"
+    puts "Place missing location data (#{location["name"]} - #{location["id"]})"
   end
 end

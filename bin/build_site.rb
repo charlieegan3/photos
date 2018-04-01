238a147b8cfc1eacc156803c708ce80c76f47924
Charlie Egan <charlieegan3@users.noreply.github.com>
2018-04-01 14:56:01 +0100

    Implement simple hugo site

---
+++ bin/build_site.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+
+require "json"
+require "yaml"
+require "date"
+require "erb"
+
+page_template = <<ERB
+<%= @data.to_yaml %>
+date: <%= DateTime.strptime(@data["timestamp"].to_s,'%s').iso8601 %>
+title: |-
+  <%= @title %>
+file_reference: <%= @file_reference %>
+---
+
+<%= format_caption(@data["caption"]) %>
+ERB
+
+def format_caption(caption)
+  if caption.split("\n").reject(&:empty?).length > 1
+    caption = caption.split("\n").each_with_index.
+      reject { |e, i| i > 0 && (e.strip.length < 3 || e[0] == "#") }.map(&:first).join("\n\n")
+  end
+
+  caption.gsub!(/(#\S+ ?)+$/, "")
+
+  caption.strip.gsub(/ \./, "")
+end
+
+def format_title(data)
+  if data["location"]
+    if data["location"]["name"]
+      return data["location"]["name"]
+    end
+  end
+
+  data["caption"] = data["caption"].split("\n").join(" ")
+
+  if data["caption"].length > 15
+    return data["caption"][0..14] + "..."
+  elsif data["caption"].length > 0
+    return data["caption"]
+  end
+
+  return "Pretty Picture"
+end
+
+def format_tags(tags)
+  tags.map { |t| t.gsub("#", "").strip }
+end
+
+`mkdir -p site/content/photos`
+
+Dir.glob("completed_json/*").shuffle.each do |file|
+  @file_reference = file.split("/").last.gsub(".json", "")
+  @data = JSON.parse(File.read(file))
+  @data["tags"] = format_tags(@data["tags"])
+  @title = format_title(@data)
+
+  markdown = ERB.new(page_template).result()
+  File.write("site/content/photos/#{@file_reference}.md", markdown)
+end

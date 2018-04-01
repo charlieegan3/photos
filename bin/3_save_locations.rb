c60d46268fba22e8f75aaad25ee36101b6bf52cb
Charlie Egan <charlieegan3@users.noreply.github.com>
2018-04-01 16:46:31 +0100

    Update location extraction

--- bin/3_save_locations.rb
+++ bin/3_save_locations.rb
@@ -3,7 +3,7 @@
 require "json"
 require "open-uri"
 
-locations_with_missing_data = %w(743759226 502239540 215736018)
+locations_with_missing_data = %w()
 
 Dir.glob("completed_json/*").map do |file|
   JSON.parse(File.read(file))["location"]
@@ -15,7 +15,9 @@ end.uniq.compact.each do |location|
 
   html = open("https://www.instagram.com/explore/locations/#{location["id"]}").read
   page_data = html.scan(/\{[^\n]+\}/).map { |r| JSON.parse(r) rescue nil }.compact.first
-  location_data = page_data.dig(*%w(entry_data LocationsPage)).first["location"]
+
+  location_data = page_data.dig(*%w(entry_data LocationsPage)).first["location"] ||
+    page_data["entry_data"]["LocationsPage"].first["graphql"]["location"]
 
   begin
     location.merge!(lat: location_data["lat"], long: location_data["lng"])

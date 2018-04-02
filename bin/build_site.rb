121644b246122c50ce56bf20760e5ff79f15300f
Charlie Egan <charlieegan3@users.noreply.github.com>
2018-04-02 02:10:51 +0100

    Show nearby photos

--- bin/build_site.rb
+++ bin/build_site.rb
@@ -75,9 +75,35 @@ Dir.glob("completed_json/*").shuffle.each do |file|
 end
 
 `rm -r site/content/locations/*`
+locations = []
 Dir.glob("locations/*").shuffle.each do |file|
-  @data = JSON.parse(File.read(file))
+  locations << JSON.parse(File.read(file))
+end
 
+def distance(point1, point2)
+  rad = 6_371_000
+  lat1, lat2, lon1, lon2 = [point1.first, point2.first, point1.last, point2.last].map { |v| v * Math::PI / 180 }
+  delta_lat = (lat1 - lat2).abs
+  delta_lon = (lon1 - lon2).abs
+
+  a = Math.sin(delta_lat / 2) * Math.sin(delta_lat / 2) +
+      Math.cos(lat1) * Math.cos(lat2) *
+      Math.sin(delta_lon / 2) * Math.sin(delta_lon / 2)
+
+  c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+
+  rad * c
+end
+
+locations.map! do |l1|
+  near = (locations - [l1]).map { |l2|
+    { "id" => l2["id"], "distance" => distance([l1["lat"], l1["long"]], [l2["lat"], l2["long"]]) }
+  }.select { |l| l["distance"] < 50000 }.sort_by { |l| l["distance"] }
+  l1.merge({"near" => near})
+end
+
+locations.each do |location|
+  @data = location
   slug = format_location_slug(@data["id"], @data["slug"])
 
   path = "site/content/locations/#{slug}"

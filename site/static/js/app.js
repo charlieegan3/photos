a6694f1aca254c8505af412cb6398fc5a617e78d
Charlie Egan <charlieegan3@users.noreply.github.com>
2018-04-09 23:32:32 +0100

    Update GA for turbolinks

--- site/static/js/app.js
+++ site/static/js/app.js
@@ -31,6 +31,43 @@ function lazyload() {
 	}
 }
 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-46126659-2', 'auto');
+
+var tracking = {
+  log: function(thing, action, label) {
+	ga("send", "event", thing, action, label, null);
+  },
+
+  logOutboundLinkClick: function(url) {
+	console.log(url);
+	if (ga.loaded) {
+	  tracking.log("outbound", "click", url);
+	}
+	var win = window.open(url, '_blank');
+	win.focus();
+  },
+
+  attachToLinks: function() {
+	var array = [];
+	var links = document.getElementsByTagName("a");
+	for(var i=0; i<links.length; i++) {
+	  if (links[i].getAttribute("href").includes("http")) {
+		links[i].setAttribute("onclick", "tracking.logOutboundLinkClick('" + links[i].href + "'); return false;");
+	  }
+	}
+  }
+}
+
+document.addEventListener("turbolinks:load", function(event) {
+  ga("set", "location", location.pathname);
+  ga("send", "pageview");
+  tracking.attachToLinks();
+});
+
 document.addEventListener("scroll", lazyload);
 document.addEventListener("turbolinks:render", lazyload);
 document.addEventListener("turbolinks:load", lazyload);

1e30dc239938038bb01f8a0a5622037788208482
Charlie Egan <charlieegan3@users.noreply.github.com>
2019-06-08 09:44:10 +0100

    Don't copy types in test

--- cli/internal/pkg/debug/debug_test.go
+++ cli/internal/pkg/debug/debug_test.go
@@ -43,27 +43,7 @@ func TestRunDebug(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	var post struct {
-		Caption    string `json:"caption"`
-		Code       string `json:"code"`
-		Dimensions struct {
-			Height int64 `json:"height"`
-			Width  int64 `json:"width"`
-		} `json:"dimensions"`
-		DisplayURL string `json:"display_url"`
-		ID         string `json:"id"`
-		IsVideo    bool   `json:"is_video"`
-		Location   struct {
-			HasPublicPage bool   `json:"has_public_page"`
-			ID            string `json:"id"`
-			Name          string `json:"name"`
-			Slug          string `json:"slug"`
-		} `json:"location"`
-		MediaURL  string   `json:"media_url"`
-		PostURL   string   `json:"post_url"`
-		Tags      []string `json:"tags"`
-		Timestamp int64    `json:"timestamp"`
-	}
+	var post types.Post
 	err = json.Unmarshal(postContent, &post)
 	if err != nil {
 		t.Error(err)

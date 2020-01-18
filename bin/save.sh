5676e1fd3dd476eee03f8d55ac0e1ba187ea16df
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-01-18 13:00:37 +0000

    Use anon email to commit

--- bin/save.sh
+++ bin/save.sh
@@ -6,6 +6,8 @@ if [ -z "$(git status --porcelain)" ]; then
   echo "Nothing to commit"
 else
   git add .
-  git -c user.name=automated -c user.email=git@charlieegan3.com commit -m "Add $(git status --porcelain | grep looted_json | wc | awk '{ print $1 }') images"
+  git -c user.name=automated \
+      -c user.email=charlieegan3@users.noreply.github.com \
+      commit -m "Add $(git status --porcelain | grep looted_json | wc | awk '{ print $1 }') images"
   git push origin master
 fi

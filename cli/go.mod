1fa72b8743f553b0f743286a948e0e88ba1a2625
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-04-14 21:33:18 +0100

    Add command to write data to remote repo

--- cli/go.mod
+++ cli/go.mod
@@ -3,9 +3,14 @@ module github.com/charlieegan3/photos
 go 1.12
 
 require (
+	github.com/go-git/go-billy/v5 v5.0.0
+	github.com/go-git/go-git/v5 v5.0.0
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/viper v1.3.2
 	github.com/urfave/cli v1.20.0 // indirect
+	gopkg.in/src-d/go-billy.v4 v4.3.2
+	gopkg.in/src-d/go-git.v4 v4.13.1
 )
---
+++ cli/internal/pkg/git/git.go
@@ -0,0 +1,73 @@
+package git
+
+import (
+	"fmt"
+	"io"
+	"strings"
+	"time"
+
+	"github.com/go-git/go-billy/v5/memfs"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/go-git/go-git/v5/storage/memory"
+	"github.com/pkg/errors"
+)
+
+// WriteToPaths will write file content to paths in a given repo
+func WriteToPaths(files map[string]string, repoURL, username, accessToken string) error {
+	fs := memfs.New()
+
+	// clone repo with credentials
+	r, err := git.Clone(memory.NewStorage(), fs, &git.CloneOptions{
+		URL:   repoURL,
+		Depth: 1,
+		Auth: &http.BasicAuth{
+			Username: username,
+			Password: accessToken,
+		},
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to clone repo")
+	}
+
+	wt, err := r.Worktree()
+	if err != nil {
+		return errors.Wrap(err, "failed load repo worktree")
+	}
+
+	for k, v := range files {
+		file, err := fs.Create(k)
+		if err != nil {
+			return errors.Wrap(err, fmt.Sprintf("failed to create file: %v", k))
+		}
+
+		if _, err = io.Copy(file, strings.NewReader(v)); err != nil {
+			return errors.Wrap(err, fmt.Sprintf("failed to write contents for file: %s", k))
+		}
+
+		_, err = wt.Add(k)
+		if err != nil {
+			return errors.Wrap(err, fmt.Sprintf("failed to stage addition for file: %s", k))
+		}
+	}
+
+	_, err = wt.Commit("add file", &git.CommitOptions{
+		Author: &object.Signature{Name: "Robot", Email: "robot@charlieegan3.com", When: time.Now()},
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to write commit")
+	}
+
+	err = r.Push(&git.PushOptions{
+		Auth: &http.BasicAuth{
+			Username: username,
+			Password: accessToken,
+		},
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to push changes")
+	}
+
+	return nil
+}

eb987edd69112309b315b73c476722841d228ec8
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-08-31 13:35:01 +0100

    Fail run if build command fails

--- hack/build_site.rb
+++ hack/build_site.rb
@@ -1,28 +1,33 @@
 #!/usr/bin/env ruby
 
+def run(command)
+  puts "running: #{command}"
+  fail unless system(command)
+end
+
 # build the cli
-system("make -C cli get-binary")
+run("make -C cli get-binary")
 # build the frontend app
-system("make -C frontend vue_build")
+run("make -C frontend vue_build")
 
 # copy app to public
-system("rm -rf public")
-system("mkdir -p public")
-system("cp -r frontend/dist/* public")
+run("rm -rf public")
+run("mkdir -p public")
+run("cp -r frontend/dist/* public")
 # make the output dir
-system("mkdir -p public/data")
+run("mkdir -p public/data")
 # generate the site data
-system("./cli/photos site debug --output public/data")
+run("./cli/photos site debug --output public/data")
 
 # commit the result
 email = `git config --global user.email`.chomp
 name = `git config --global user.name`.chomp
 if name == "" || email == ""
   puts "setting gh actions git identity"
-  system('git config --global user.email "githubactions@example.com"')
-  system('git config --global user.name "GitHub Actions"')
+  run('git config --global user.email "githubactions@example.com"')
+  run('git config --global user.name "GitHub Actions"')
 end
-system("git checkout -b netlify")
-system("git add public")
-system("git -c commit.gpgsign=false commit -m generate-site")
-system("git push -f origin netlify")
+run("git checkout -b netlify")
+run("git add public")
+run("git -c commit.gpgsign=false commit -m generate-site")
+run("git push -f origin netlify")

abaeea74c3aa5e32a70f895db94db1e6fd40d09d
Charlie Egan <charlieegan3@users.noreply.github.com>
2021-11-09 18:14:50 +0000

    Allow disabling of proxy

--- cli/internal/pkg/proxy/proxy.go
+++ cli/internal/pkg/proxy/proxy.go
@@ -12,6 +12,7 @@ import (
 
 var proxyURLRaw string
 var proxyToken string
+var proxyEnable bool
 
 func init() {
 	proxyURLRaw = os.Getenv("PROXY_URL")
@@ -32,6 +33,12 @@ func init() {
 		log.Fatal("PROXY_TOKEN is not set")
 		os.Exit(1)
 	}
+
+	proxyEnableStr := os.Getenv("PROXY_ENABLE")
+	proxyEnable = true
+	if proxyEnableStr == "false" {
+		proxyEnable = false
+	}
 }
 
 // GetURLViaProxy will make a get request using a simple-proxy endpoint
@@ -40,24 +47,33 @@ func GetURLViaProxy(requestURL string, headers map[string]string) (int, []byte, 
 	log.Printf("fetching via proxy: %s", requestURL)
 	response := &http.Response{}
 
-	// need a fresh copy of the url since we change the RawQuery each request
-	proxyURL, err := url.Parse(proxyURLRaw)
-	if err != nil {
-		return 0, []byte{}, errors.Wrap(err, "failed to parse raw proxy url")
+	var urlToRequest string
+	if proxyEnable {
+		// need a fresh copy of the url since we change the RawQuery each request
+		proxyURL, err := url.Parse(proxyURLRaw)
+		if err != nil {
+			return 0, []byte{}, errors.Wrap(err, "failed to parse raw proxy url")
+		}
+
+		q := proxyURL.Query()
+		q.Add("url", requestURL)
+		proxyURL.RawQuery = q.Encode()
+
+		urlToRequest = proxyURL.String()
+	} else {
+		urlToRequest = requestURL
 	}
 
-	q := proxyURL.Query()
-	q.Add("url", requestURL)
-	proxyURL.RawQuery = q.Encode()
-
-	req, err := http.NewRequest("GET", proxyURL.String(), nil)
+	req, err := http.NewRequest("GET", urlToRequest, nil)
 	if err != nil {
 		return 0, []byte{}, errors.Wrap(err, "failed to create proxy request")
 	}
 
 	client := &http.Client{}
 
-	req.Header.Add("Authorization", "bearer "+proxyToken)
+	if proxyEnable {
+		req.Header.Add("Authorization", "bearer "+proxyToken)
+	}
 
 	for k, v := range headers {
 		req.Header.Add(k, v)
@@ -65,7 +81,11 @@ func GetURLViaProxy(requestURL string, headers map[string]string) (int, []byte, 
 
 	response, err = client.Do(req)
 	if err != nil {
-		return 0, []byte{}, errors.Wrap(err, "failed to get via proxy")
+		if proxyEnable {
+			return 0, []byte{}, errors.Wrap(err, "failed to get via proxy")
+		} else {
+			return 0, []byte{}, errors.Wrap(err, "failed to get url")
+		}
 	}
 	defer response.Body.Close()
 	body, err := ioutil.ReadAll(response.Body)

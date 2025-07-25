7a6b4dfd0e1f9433aeb4dddf6749d8c74fa189f4
Charlie Egan <charlieegan3@users.noreply.github.com>
2025-07-25 14:05:08 +0100

    add-flake

---
+++ flake.nix
@@ -0,0 +1,104 @@
+{
+  description = "photos";
+
+  inputs = {
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      rev = "fc02ee70efb805d3b2865908a13ddd4474557ecf";
+    };
+    flake-utils = {
+      type = "github";
+      owner = "numtide";
+      repo = "flake-utils";
+      rev = "b1d9ab70662946ef0850d488da1c9019f3a9752a";
+    };
+    pre-commit-hooks = {
+      type = "github";
+      owner = "cachix";
+      repo = "git-hooks.nix";
+      rev = "c7012d0c18567c889b948781bc74a501e92275d1";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    let
+      utils = flake-utils;
+    in
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+
+      in
+      {
+        checks = {
+          pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              dprint = {
+                enable = true;
+                name = "dprint check";
+                entry = "dprint check --allow-no-files";
+              };
+              nixfmt = {
+                enable = true;
+                name = "nixfmt check";
+                entry = "nixfmt -c ";
+                types = [ "nix" ];
+              };
+            };
+          };
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+
+        packages.default = pkgs.buildGoModule {
+          pname = "photos";
+          version = "0.1.0";
+          vendorHash = "sha256-pp76jAHxlZx4RG9iaG1GboajJhfWj8h8+K1u7DGCSuw=";
+          src = ./.;
+          checkPhase = "";
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+          buildInputs = with pkgs; [
+            pkg-config
+          ];
+        };
+
+        devShells = {
+          default = pkgs.mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+            buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+
+            env = {
+              DOCKER_HOST = "unix:///Users/charlieegan3/.colima/default/docker.sock";
+              TESTCONTAINERS_RYUK_DISABLED = "true";
+            };
+
+            packages = with pkgs; [
+              go
+              golangci-lint
+
+              exiftool
+
+              dprint
+              nixfmt-rfc-style
+              claude-code
+            ];
+          };
+        };
+      }
+    );
+}
